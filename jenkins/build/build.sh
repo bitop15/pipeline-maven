7e45ef291fe5c4d9ce4910d91b0c465825c86ee8
Gustavo <gtorresm.ids@gmail.com>
2022-12-06 17:55:14 -0600

    commit

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -7,4 +7,5 @@ echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
+
 cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
