2b4a3aaa30b53f2f67e1f6e2db52310c36974345
Gustavo <gtorresm.ids@gmail.com>
2022-12-06 17:39:40 -0600

    commit

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,8 +3,8 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /Users/idscomercial/jenkins/prod /tmp/.auth ubuntu@ec2-3-82-149-71.compute-1.amazonaws.com:/tmp/.auth
-scp -i /Users/idscomercial/jenkins/prod ./jenkins/deploy/publish.sh ubuntu@ec2-3-82-149-71.compute-1.amazonaws.com:/tmp/publish
+scp -i /opt/prod /tmp/.auth ubuntu@ec2-3-82-149-71.compute-1.amazonaws.com:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish.sh ubuntu@ec2-3-82-149-71.compute-1.amazonaws.com:/tmp/publish
 
 
-ssh -i /Users/idscomercial/jenkins/prod ubuntu@ec2-3-82-149-71.compute-1.amazonaws.com "/tmp/publish"
+ssh -i /opt/prod ubuntu@ec2-3-82-149-71.compute-1.amazonaws.com "/tmp/publish"
